d470ea3801776c26733fa17c88f348a515450802
celtixtime <celtixtime@gmail.com>
2017-02-08 09:34:30 +0100

    Built expr arith builder

--- src/pExpr/pNonTerminal/pEnsemble/Ensemble.java
+++ src/pExpr/pNonTerminal/pEnsemble/Ensemble.java
@@ -1,8 +1,14 @@
 package pExpr.pNonTerminal.pEnsemble;
 import pExpr.pNonTerminal.NonTerminal;
+import pExpr.pNonTerminal.pSousExpr.pExprArith.ExprArith;
+
+import java.util.ArrayList;
 
 public abstract class Ensemble extends NonTerminal {
-
+    ExprArith[] ensemble;
+    public Ensemble(ExprArith ... expr){
+        ensemble=expr;
+    }
 }
 
 // Ensemble	::=	{ ExprArith (, ExprArith)* }
