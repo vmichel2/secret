14a292ee8caf4a76f4fbada99aaabda0385a5e93
Celtixtime <Celtixtime@gmail.com>
2017-02-10 16:49:07 +0100

    Trying main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
-public class Main {
+import pExpr.Expression;
+import pExpr.pNonTerminal.pComparaisons.pComparaisonArithmetique.Egalite;
+import pExpr.pNonTerminal.pComparaisons.pComparaisonArithmetique.Plusgrandque;
+import pExpr.pNonTerminal.pEnsemble.Ensemble;
+import pExpr.pNonTerminal.pSousExpr.pExprEnsembliste.AppartientA;
+import pExpr.pNonTerminal.pSousExpr.pExprLogique.Etlogique;
+import pExpr.pNonTerminal.pSousExpr.pExprLogique.QuelqueSoit;
+import pExpr.pTerminal.Entier;
+import pExpr.pTerminal.Litteral;
 
+public class Main {
+    public void main (String args[]){
+        Litteral x = new Litteral("x");
+        Litteral X = new Litteral("X");
+        Expression exemple1 = new Etlogique(
+                new Egalite(X, new Ensemble(new Entier(1), new Entier(2), new Entier(3))),
+                new QuelqueSoit(x, new AppartientA(x, X), new Plusgrandque(x, new Entier(0))));
+    }
 }
--- src/pExpr/pNonTerminal/pEnsemble/Ensemble.java
+++ src/pExpr/pNonTerminal/pEnsemble/Ensemble.java
@@ -1,10 +1,9 @@
 package pExpr.pNonTerminal.pEnsemble;
 import pExpr.Expression;
 import pExpr.pNonTerminal.NonTerminal;
-import pExpr.pTerminal.Litteral;
 
 
-public abstract class Ensemble extends NonTerminal {
+public class Ensemble extends NonTerminal {
     Expression[] ensemble;
     public Ensemble(Expression... expr){
         ensemble=expr;
