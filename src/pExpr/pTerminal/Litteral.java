440f7c1338f7510428898dca2caae2ccf941a75d
celtixtime <celtixtime@gmail.com>
2017-02-14 11:37:24 +0100

    No need to change litteal thingy !

--- src/pExpr/pTerminal/Litteral.java
+++ src/pExpr/pTerminal/Litteral.java
@@ -8,7 +8,6 @@ public class Litteral extends Terminal {
         this.litteral=litteral;
     }
 
-
     public void afficher(String prefixe) {
         System.out.println(prefixe + "Litteral : " + this.litteral);
     }
